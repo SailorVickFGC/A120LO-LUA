f0df88c7a07dbc806bfc94b0b9f95f7a4470ea85
Vick <victor-ge@outlook.com>
2021-08-09 19:14:09 +0200

    Add the in-game Hitbox Viewer!!
    Thanks to @serubis for telling us there's a hitbox viewer and how to unlock it!
    I also stole CriCri code to get this working, so Thanks CriCri x]  .

--- A120LO-Training.lua
+++ A120LO-Training.lua
@@ -4,7 +4,7 @@ local DMG
 local LastDMG = 0
 
 local StunAddr=0x087342
-local Stun
+local Stun = memory.readbyte(StunAddr)
 
 local StunLimP2Addr=0x087348
 local StunLimP2
@@ -51,28 +51,32 @@ local LastComboAddr = 0x086E12
 local LastCombo
 
 
---- CODE ---
-while true do
--- Combo Counter
+function comboCounter()
 	Combo=memory.readbyte(ComboAddr)
 	LastCombo=memory.readbyte(LastComboAddr)
 	gui.text(10,200, "Combo:" .. Combo .. " (" .. LastCombo .. ")")
 	-- gui.text(10,215, "Last Combo:" .. LastCombo)
 
--- Combo Damage
+end
+
+function comboDamage()
 	DMG=191 - memory.readbyte(DMGAddr) -- Starts at -1 but once you hit it come back to 0 (-1hp in training for some reasons)
 	if DMG ~= 0 then LastDMG=DMG
 	end
 	gui.text(10,240,"Damage: " .. DMG .. " (" .. LastDMG .. ") ")
 
--- Stun
+end
+
+function stunCounter()
 	Stun=memory.readbyte(StunAddr)
 	StunLimP2=memory.readbyte(StunLimP2Addr)
 	StunTimer=memory.readbyte(StunTimerAddr)
 	gui.text(10,255,"Stun: " .. Stun .. "/" .. StunLimP2 .. " - " .. StunTimer)
+end
 
--- P2 State
+function dummyState()
 	P2State=memory.readbyte(P2StateAddr)
+
 	if P2State==0 then 
 		gui.text(10,270,"P2 State: Hit")
 	end
@@ -88,6 +92,87 @@ while true do
 	if P2State==15 then
 		gui.text(10,270, "P2 State: Idle") 
 	end
+end
+
+
+
+-- Asuka 120% L.O. Pause Menu class --
+HitboxViewer = {}
+
+-- Initializing
+function HitboxViewer:new()
+    setmetatable({}, HitboxViewer)
+
+    self.m_menu = 0x0FFB82
+    self.m_pause = 0x07984A
+    self.m_modeSelect = 0x07C9B6
+    self.m_isEnabled = false
+		
+		return self
+end
+
+
+-- Asuka 120% L.O. Pause Menu class --
+PauseMenu = {}
+
+-- Initializing
+function PauseMenu:new()
+    setmetatable({}, PauseMenu)
+
+    self.m_menu = 0x0FFB82
+    self.m_pause = 0x07984A
+    self.m_modeSelect = 0x07C9B6
+    self.m_isEnabled = false
+
+    return self
+end
+
+--[[Return the value of the selected menu :
+- 99 is the main menu
+- 1 is VS Game
+- 2 is Ranking mode
+- 3 is Deathmatch
+- 4 is Config 
+- 150 is the battle screen (playing)]]
+function PauseMenu:menuSelected()
+    menuSelect = memory.readbyte(self.m_menu)
+    return menuSelect
+end
+
+function PauseMenu:pauseIsActive()
+    pauseEnabled = memory.readbyte(self.m_pause)
+    return pauseEnabled
+end
+
+--[[Return the value of the select Vs Mode :
+- 0 is 1p vs 2p
+- 1 is vs com 
+- 2 is com vs com 
+- 3 is deku]]
+function PauseMenu:modeSelected()
+    vsModeSelected = memory.readbyte(self.m_modeSelect)
+    return vsModeSelected
+end
+
+Pause = PauseMenu:new()
+-- Hitbox viewer enable if both = 1
+-- 0x0856B0 
+-- 0x090402
+
+while true do
+	if Pause:menuSelected() == 150 and Pause:modeSelected() then
+		memory.writebyte(0x0856B0, 1)
+		memory.writebyte(0x090402, 1)
+	else
+		memory.writebyte(0x0856B0, 0)
+		memory.writebyte(0x090402, 0)
+	end
+
+
+	comboCounter()
+	comboDamage()
+	stunCounter()
+	dummyState()
 
 	emu.frameadvance()
 end
